83e60fe39bf56148706d55307cafca83d13f01ad
n-capuano <nicolas.capuano.dev@gmail.com>
2020-11-20 13:04:03 +0100

    page apiSelector ok

---
+++ public/assets/js/map.js
@@ -0,0 +1,13 @@
+const wiki = document.getElementById("wiki-btn");
+const warsim = document.getElementById("warsim-btn");
+const travel = document.getElementById("travel-btn");
+const quote = document.getElementById("quote-btn");
+const weather = document.getElementById("weather-btn");
+const messageBox = document.getElementById("messageBox");
+
+wiki.addEventListener("mouseover", function( event ){
+wiki.setAttribute("src","../images/howbutton2.png");
+var text =  "Wiki";
+
+
+})
